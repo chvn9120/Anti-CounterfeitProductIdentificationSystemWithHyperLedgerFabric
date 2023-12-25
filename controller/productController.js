b11b34877ae4b43811d939d409e3747ab400d9d6
WiBuMoney <cowsmile1911@gmail.com>
2023-12-25 18:57:17 +0700

    update add2cart

--- controller/productController.js
+++ controller/productController.js
@@ -3,6 +3,7 @@ import { validationResult, matchedData } from 'express-validator'
 
 import ProductBase from '../models/product.js'
 import BrandBase from '../models/brand.js'
+import CartBase from '../models/cart.js';
 
 const GetProductById = async (req, res, next) => {
 	const result = validationResult(req);
@@ -12,23 +13,71 @@ const GetProductById = async (req, res, next) => {
 		const filter = { p_id: id };
 
 		const found = await ProductBase.findOne(filter).lean();
-		const brand = await BrandBase.findOne({ _id: found.brand_id }).lean();
 
 		if (!found) {
 			req.session.error_404 = `Please check your ID for the product again!`;
-			return next(createError(404, `Error! The roduct with id=${id} does not exist!`));
+			return next(createError(404, `Error! The product with id=${id} does not exist!`));
 		}
+
+		const brand = await BrandBase.findOne({ _id: found.brand_id }).lean();
 
 		req.session.product = found;
 		const currentUser = req.session.user || req.cookies.user;
+		const cart = await CartBase.findOne({ owner: currentUser._id }).lean();
+		let qtyInCart;
+		if (cart !== null) {
+			qtyInCart = cart.product_and_quantity.length;
+		}
 
-		res.render('p_detail', {
-			product: found,
-			brand,
-			user: currentUser,
-			header: 'header',
-			footer: 'footer'
-		});
+		if (currentUser !== undefined) {
+			res.render('p_detail', {
+				product: found,
+				brand,
+				user: currentUser,
+				qtyInCart,
+				header: 'header',
+				footer: 'footer'
+			});
+		} else {
+			if (req.session.isLogin) {
+				res.render('p_detail', {
+					product: found,
+					brand,
+					user: currentUser,
+					qtyInCart,
+					header: 'header',
+					footer: 'footer'
+				});
+			} else {
+				if (req.session.orderRoute) {
+					res.render('p_detail', {
+						product: found,
+						brand,
+						displayModal: true,
+						orderFunc: true,
+						header: 'header',
+						footer: 'footer'
+					});
+				} else if (req.session.cartRoute) {
+					res.render('p_detail', {
+						product: found,
+						brand,
+						displayModal: true,
+						cartFunc: true,
+						header: 'header',
+						footer: 'footer'
+					});
+				} else {
+					res.render('p_detail', {
+						product: found,
+						brand,
+						header: 'header',
+						footer: 'footer'
+					});
+				}
+			}
+		}
+
 		return;
 	}
 
